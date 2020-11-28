b73db3be27ff1e91d276ee5d174442867285cec9
mausworks <rasmuswennerstrom@gmail.com>
2020-11-28 23:11:14 +0100

    🖌 implement stats API

--- src/UserLink.svelte
+++ src/UserLink.svelte
@@ -1,28 +1,37 @@
 <script>
   import { scale } from "svelte/transition";
+  import { fetchStats } from "./api";
 
   export let url;
   export let slug;
 
+  let visitCount;
+  let lastVisit;
+
   export let clickDiscard = () => {};
   export let clickDelete = () => {};
+
+  const clickShowVisits = () => {
+    fetchStats(slug).then((stats) => {
+      visitCount = stats.count;
+      lastVisit = stats.lastVisitedAt;
+    });
+  };
 </script>
 
 <style>
   .user-link {
     position: relative;
     display: flex;
+    flex-direction: column;
     padding: 12px 0;
     margin: 0 6px;
-    border-top: 1px dashed #ddd;
+    border-bottom: 1px dashed #ddd;
   }
 
-  .user-link:first-child {
-    border-top: none;
-  }
-
-  .submission {
-    flex: 1 0 auto;
+  .user-link:last-child {
+    border-bottom: none;
+    padding-bottom: 0;
   }
 
   .origin {
@@ -55,66 +64,87 @@
 
   .actions {
     display: flex;
-    flex-direction: column;
-    flex: 0 0 auto;
-    justify-content: center;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 14px 0 6px 0;
   }
 
   .actions button {
+    font-family: "Folio Medium", sans-serif;
+    border-radius: 500px;
     border: none;
-    top: 0;
-    right: 0;
-    padding: 0;
+    padding: 12px 14px 10px 14px;
+    line-height: 0.5;
+    font-size: 14px;
+    background: #efefef;
+  }
+
+  .actions button.visits-loaded {
+    background: none;
+  }
+
+  .actions .delete {
+    margin-right: 8px;
+    background-color: #d10000;
+    color: #fff;
+  }
+
+  .discard {
+    border: none;
+    margin: 12px 0;
     height: 24px;
     width: 24px;
+    position: absolute;
+    top: 0;
+    right: 0;
     display: flex;
     align-items: center;
     justify-content: center;
     background: none;
   }
 
-  .actions svg {
+  .discard svg {
     width: 12px;
     height: 12px;
-  }
-
-  .delete svg {
-    fill: #d10000;
   }
 </style>
 
 <div class="user-link" in:scale out:scale>
   <div class="submission">
-    <input
-      class="origin"
-      readonly
-      onclick="this.select();"
-      type="text"
-      value="https://maus.to/{slug}" />
-    <div class="target">
-      <svg viewBox="0 0 24 24">
-        <path d="M16 8v-4l8 8-8 8v-4h-16l8-8h8z" />
-      </svg>
-      <span>{url}</span>
+    <div class="info">
+      <input
+        class="origin"
+        readonly
+        onclick="this.select();"
+        type="text"
+        value="https://maus.to/{slug}" />
+
+      <div class="target">
+        <svg viewBox="0 0 24 24">
+          <path d="M16 8v-4l8 8-8 8v-4h-16l8-8h8z" />
+        </svg>
+        <span>{url}</span>
+      </div>
     </div>
   </div>
 
-  <div class="actions">
-    <button
-      class="discard"
-      title="Remove from your links"
-      on:click={clickDiscard}>
-      <svg viewBox="0 0 24 24" clip-rule="evenodd" fill-rule="evenodd">
-        <path
-          d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z" />
-      </svg>
-    </button>
+  <button
+    class="discard"
+    title="Remove from your links"
+    on:click={clickDiscard}>
+    <svg viewBox="0 0 24 24" clip-rule="evenodd" fill-rule="evenodd">
+      <path
+        d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z" />
+    </svg>
+  </button>
 
-    <button class="delete" title="Delete link" on:click={clickDelete}>
-      <svg viewBox="0 0 24 24" fill-rule="evenodd" clip-rule="evenodd">
-        <path
-          d="M19 24h-14c-1.104 0-2-.896-2-2v-17h-1v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2h-1v17c0 1.104-.896 2-2 2zm-14-2.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-16.5h-14v16.5zm5-18.5h4v-1h-4v1z" />
-      </svg>
+  <div class="actions">
+    <button class="delete" on:click={clickDelete}>Delete</button>
+    <button
+      title={lastVisit && `Last visited ${new Date(lastVisit).toLocaleString()}`}
+      on:click={clickShowVisits}
+      class={visitCount !== undefined ? 'visits-loaded' : ''}>
+      {visitCount === undefined ? 'Show visits' : visitCount === 1 ? `1 visit` : `${visitCount} visits`}
     </button>
   </div>
 </div>
